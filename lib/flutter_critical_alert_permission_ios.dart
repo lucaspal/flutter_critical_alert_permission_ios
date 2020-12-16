2a7e21150327eb4060ae69727376054fbe51ca9b
Luca Spalierno <spalierno@gmail.com>
2020-12-16 15:56:08 +0100

    Add try catch.

--- lib/flutter_critical_alert_permission_ios.dart
+++ lib/flutter_critical_alert_permission_ios.dart
@@ -1,5 +1,6 @@
 
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/services.dart';
 
@@ -8,6 +9,10 @@ class FlutterCriticalAlertPermissionIos {
       const MethodChannel('flutter_critical_alert_permission_ios');
 
   static Future<void> requestCriticalAlertPermission() async {
-    await _channel.invokeMethod('requestCriticalAlertPermission');
+    try {
+      await _channel.invokeMethod('requestCriticalAlertPermission');
+    } on Exception {
+      log('Cannot request permission');
+    }
   }
 }
